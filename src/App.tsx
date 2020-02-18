fae9a4ef6cbb59f616b10a6c7ffd953d92f3b534
Natali Chornenko <nataliii.chornenko@gmail.com>
2020-02-18 21:58:30 +0200

    Need to improve

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => {
 
 
             <div className={'build-block'}>
-                <div className={'prosto'}>
+                <div>
                     <BlockMenu/>
                 </div>
                 <div className={'field-inform'}>
